62017b1734c50508bb7fc944f0ac610d149e707a
jst.talel <mathewstalel@outlook.com>
2026-02-09 16:13:54 +0300

    GR11-2: setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Profile from './pages/Profile'
+
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/about">About</Link> |
+        <Link to="/projects">Projects</Link> |
+        <Link to="/contact">Contact</Link> |
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
   )
 }
-
 export default App
