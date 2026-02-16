d5c819c000b52c584660c34b0176420c21f000ee
roy9 <roymurimi527@gmail.com>
2026-02-16 13:46:36 +0300

    GR11-3-Create-Home-page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Hi, I’m Roy 👋</h1>
+      <p>
+        I’m a software engineering student passionate about building
+        clean and functional web applications.
+      </p>
+
+      <button>View My Projects</button>
+    </div>
+  )
+}
+
+export default Home
