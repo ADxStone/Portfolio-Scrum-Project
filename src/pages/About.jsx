3b2378719891ebb6a6563f2b5a8fc9e0606d669b
roy9 <roymurimi527@gmail.com>
2026-02-16 13:56:55 +0300

    GR11-4-Create-About-page

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+
+      <p>
+        I am a software engineering student with experience in
+        JavaScript, React, and modern web development tools.
+      </p>
+
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML & CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Git & GitHub</li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
